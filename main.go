66c45976b0520e27d6551633d4272556a72ab206
evanhydec <evanhydec@163.com>
2022-06-11 20:39:02 +0800

    final

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/RaymondCode/simple-demo/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,5 +10,9 @@ func main() {
 
 	initRouter(r)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if err := r.Run(); err != nil {
+		utils.LogrusObj.Info(err)
+		panic(err)
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
 }
